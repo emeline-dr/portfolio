44de2a1a89185c9eaefa6db2b742ed9261542a73
Emeline De Roo <deroo.emeline@gmail.com>
2025-09-29 16:07:22 +0200

    Retour en arrière

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,23 +1,16 @@
 import { getRequestConfig } from 'next-intl/server';
 import { cookies } from 'next/headers';
 
-export default getRequestConfig(async ({ locale, headers }) => {
+export default getRequestConfig(async ({ locale }) => {
   const supportedLocales = ['en', 'fr'];
 
-  // Récupère la valeur du cookie
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const cookieLocale = cookieStore.get('NEXT_LOCALE')?.value;
-
-  // Détecte la langue du navigateur via Accept-Language
-  const acceptLanguage = headers.get('accept-language') || '';
-  const browserLocale = acceptLanguage.split(',')[0].split('-')[0]; // ex: "fr-FR" → "fr"
 
   const currentLocale =
     cookieLocale && supportedLocales.includes(cookieLocale)
       ? cookieLocale
-      : supportedLocales.includes(browserLocale)
-        ? browserLocale
-        : locale || 'en'; // fallback si aucune correspondance
+      : locale || 'en';
 
   return {
     locale: currentLocale,
