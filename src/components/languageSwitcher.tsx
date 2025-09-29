e817b374da13ef8776017f8ca371c4fa98b58fd5
Emeline De Roo <deroo.emeline@gmail.com>
2025-09-29 16:25:12 +0200

    Test

--- src/components/languageSwitcher.tsx
+++ src/components/languageSwitcher.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import "flag-icons/css/flag-icons.min.css";
-import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 export default function LanguageSwitcher() {
     const [cookieLocale, setCookieLocale] = useState<string | undefined>();
@@ -14,12 +14,13 @@ export default function LanguageSwitcher() {
         } else {
             const browserLang = navigator.language.startsWith("fr") ? "fr" : "en";
             setCookieLocale(browserLang);
-            Cookies.set("NEXT_LOCALE", browserLang, { path: "/", expires: 365 });
+            // Appeler le endpoint côté serveur pour créer le cookie
+            fetch(`/api/set-locale?locale=${browserLang}`);
         }
     }, []);
 
-    const switchLanguage = (lang: "en" | "fr") => {
-        Cookies.set("NEXT_LOCALE", lang, { path: "/", expires: 365 });
+    const switchLanguage = async (lang: "en" | "fr") => {
+        await fetch(`/api/set-locale?locale=${lang}`);
         window.location.reload();
     };
 
@@ -27,16 +28,14 @@ export default function LanguageSwitcher() {
         <div className="absolute flex content-center h-[80] end-[80] top-0">
             <button
                 onClick={() => switchLanguage("en")}
-                className={`h-[24] self-center me-[16] ${cookieLocale === "en" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"
-                    }`}
+                className={`h-[24] self-center me-[16] ${cookieLocale === "en" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"}`}
                 disabled={cookieLocale === "en"}
             >
                 <span className="fi fi-gb fis h-[24] rounded-sm"></span>
             </button>
             <button
                 onClick={() => switchLanguage("fr")}
-                className={`h-[24] self-center ${cookieLocale === "fr" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"
-                    }`}
+                className={`h-[24] self-center ${cookieLocale === "fr" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"}`}
                 disabled={cookieLocale === "fr"}
             >
                 <span className="fi fi-fr fis h-[24] rounded-sm"></span>
