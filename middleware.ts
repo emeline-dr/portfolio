34654565f19cef9fb7aa4a3bb1a0a95c3ed4ba92
Emeline De Roo <deroo.emeline@gmail.com>
2025-09-29 14:59:08 +0200

    Ajout du prefic

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['fr', 'en'],
   defaultLocale: 'fr',
+  localePrefix: 'as-needed'
 });
 
 export const config = {
