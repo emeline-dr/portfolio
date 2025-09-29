b54cba8cf0353278dbfa376a7f1005e783e229fc
Emeline De Roo <deroo.emeline@gmail.com>
2025-09-29 16:35:55 +0200

    Test

--- src/components/languageSwitcher.tsx
+++ src/components/languageSwitcher.tsx
@@ -1,26 +1,21 @@
 'use client';
 
 import "flag-icons/css/flag-icons.min.css";
-import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 
 export default function LanguageSwitcher() {
     const [cookieLocale, setCookieLocale] = useState<string | undefined>();
 
     useEffect(() => {
-        const localeCookie = Cookies.get("NEXT_LOCALE");
-        if (localeCookie) {
-            setCookieLocale(localeCookie);
-        } else {
-            const browserLang = navigator.language.startsWith("fr") ? "fr" : "en";
-            setCookieLocale(browserLang);
-            // Appeler le endpoint côté serveur pour créer le cookie
-            fetch(`/api/set-locale?locale=${browserLang}`);
+        setCookieLocale(Cookies.get("NEXT_LOCALE"));
+        if (!Cookies.get("NEXT_LOCALE")) {
+            Cookies.set("NEXT_LOCALE", 'en', { path: "/", expires: 365 })
         }
     }, []);
 
-    const switchLanguage = async (lang: "en" | "fr") => {
-        await fetch(`/api/set-locale?locale=${lang}`);
+    const switchLanguage = (lang: "en" | "fr") => {
+        Cookies.set("NEXT_LOCALE", lang, { path: "/", expires: 365 });
         window.location.reload();
     };
 
@@ -28,14 +23,16 @@ export default function LanguageSwitcher() {
         <div className="absolute flex content-center h-[80] end-[80] top-0">
             <button
                 onClick={() => switchLanguage("en")}
-                className={`h-[24] self-center me-[16] ${cookieLocale === "en" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"}`}
+                className={`h-[24] self-center me-[16] ${cookieLocale === "en" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"
+                    }`}
                 disabled={cookieLocale === "en"}
             >
                 <span className="fi fi-gb fis h-[24] rounded-sm"></span>
             </button>
             <button
                 onClick={() => switchLanguage("fr")}
-                className={`h-[24] self-center ${cookieLocale === "fr" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"}`}
+                className={`h-[24] self-center ${cookieLocale === "fr" ? "opacity-25 cursor-not-allowed" : "cursor-pointer"
+                    }`}
                 disabled={cookieLocale === "fr"}
             >
                 <span className="fi fi-fr fis h-[24] rounded-sm"></span>
