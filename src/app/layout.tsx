305095d3e6d9c6f2c979f893a5830436dbcfa037
Emeline De Roo <deroo.emeline@gmail.com>
2025-09-29 15:08:54 +0200

    Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { NextIntlClientProvider } from "next-intl";
-import { notFound } from "next/navigation";
-
-async function getMessages(locale: string) {
-  try {
-    return (await import(`../locales/${locale}.json`)).default;
-  } catch (error) {
-    notFound();
-  }
-}
 
 export const metadata: Metadata = {
   title: "Emeline DR",
   description: "Portfolio d'Emeline De Roo",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const locale = "fr";
-
-  const messages = await getMessages(locale);
-
   return (
-    <html lang={locale}>
-      <body className="antialiased">
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
-        </NextIntlClientProvider>
+    <html lang="fr">
+      <body
+        className="antialiased"
+      >
+        {children}
       </body>
     </html>
   );
