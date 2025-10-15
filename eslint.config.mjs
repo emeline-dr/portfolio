81aa4c989a1917b50c8a5d848df9552a24f33ab7
Emeline De Roo <deroo.emeline@gmail.com>
2025-10-15 13:50:15 +0200

    Désactivation no-explicit-any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,11 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  ...compat.config({
+    rules:{
+      'typescript-eslint/no-explicit-any': 'off'
+    }
+  })
 ];
 
 export default eslintConfig;
